856eb3593fd2f72c52bfffee095a7b7f45d31e33
h3nnn4n <uber.renan@gmail.com>
2016-01-07 21:04:08 -0200

    Arduino sketch

---
+++ nesduino/nesduino.ino
@@ -0,0 +1,34 @@
+int strobe   = 9;
+int clock   = 8;
+int data    = 7;
+int buttons = 0;
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(strobe, OUTPUT);
+    pinMode(clock, OUTPUT);
+    pinMode(data,  INPUT);
+
+    digitalWrite(strobe, LOW);
+    digitalWrite(clock, LOW);
+}
+
+void loop() {
+    buttons = 0;
+
+    digitalWrite(strobe, HIGH);
+    digitalWrite(strobe, LOW);
+
+    buttons = digitalRead(data);
+
+    for (int i = 0 ; i < 12 ; i++) {
+        digitalWrite(clock, HIGH);
+        buttons = buttons << 1;
+        buttons = buttons + digitalRead(data) ;
+        digitalWrite(clock, LOW);
+    }
+
+    Serial.println(buttons, BIN);
+}
+
+
